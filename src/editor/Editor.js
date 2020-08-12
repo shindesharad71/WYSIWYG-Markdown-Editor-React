b85e1baedfafcc1f3b3d334237781cea5186ea8b
Sharad Shinde <sharad.shinde@coditas.com>
2020-08-12 20:30:06 +0530

    Added: Bold

--- src/editor/Editor.js
+++ src/editor/Editor.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import * as marked from 'marked';
 import './Editor.css';
 
 export default class Editor extends Component {
@@ -6,30 +7,53 @@ export default class Editor extends Component {
 		super(props);
 		this.state = {
 			value: 'Remember, be nice!',
-        };
+			htmlState: '<p>Abc</p>',
+		};
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSelection = this.handleSelection.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSelection = this.handleSelection.bind(this);
+		this.preview = this.preview.bind(this);
+		this.makeBold = this.makeBold.bind(this);
 	}
 
 	handleChange(event) {
-        this.setState({ value: event.target.value });
-    }
+		this.setState({ value: event.target.value });
+	}
 
-    handleSelection() {
-        console.log(window.getSelection().toString());
-    }
+	handleSelection() {
+		console.log(window.getSelection().toString());
+	}
+
+	preview() {
+		this.setState({ htmlState: marked(this.state.value) });
+	}
+
+	makeBold() {
+		this.setState({
+			value: this.state.value.replace(
+				window.getSelection().toString(),
+				`**${window.getSelection().toString()}**`
+			),
+		});
+	}
 
 	render() {
 		return (
-			<textarea
-				cols="100"
-				rows="20"
-				wrap="off"
-				value={this.state.value}
-                onChange={this.handleChange}
-                onMouseUp={this.handleSelection}
-			></textarea>
+			<div>
+				<button onClick={this.preview}>Show Output</button>
+				<button onClick={this.makeBold}>bold</button>
+				<div
+					dangerouslySetInnerHTML={{ __html: this.state.htmlState }}
+				></div>
+				<textarea
+					cols="100"
+					rows="20"
+					wrap="off"
+					value={this.state.value}
+					onChange={this.handleChange}
+					onMouseUp={this.handleSelection}
+				></textarea>
+			</div>
 		);
 	}
 }
