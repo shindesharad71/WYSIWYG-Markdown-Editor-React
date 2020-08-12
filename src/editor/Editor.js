9f2c72f99731530a0ff3feef1e5aee6172ee1526
Sharad Shinde <sharad.shinde@coditas.com>
2020-08-12 19:30:36 +0530

    Updated: Textarea Style

--- src/editor/Editor.js
+++ src/editor/Editor.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './Editor.css';
 
 export default class Editor extends Component {
 	render() {
 		return (
-			<textarea
+			<textarea id="editor"
 				placeholder="Remember, be nice!"
 				cols="100"
                 rows="20"
