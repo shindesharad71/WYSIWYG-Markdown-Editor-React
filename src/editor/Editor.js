d4e40550d3a7231a7aaee061bbaf08bf612cf5fb
Sharad Shinde <sharad.shinde@coditas.com>
2020-08-12 19:33:28 +0530

    Updated: Integrated React Form

--- src/editor/Editor.js
+++ src/editor/Editor.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import './Editor.css';
 
 export default class Editor extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: 'Remember, be nice!',
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({ value: event.target.value });
+	}
+
 	render() {
 		return (
-			<textarea id="editor"
-				placeholder="Remember, be nice!"
+			<textarea
+				id="editor"
 				cols="100"
-                rows="20"
-                wrap="off"
+				rows="20"
+				wrap="off"
+				value={this.state.value}
+				onChange={this.handleChange}
 			></textarea>
 		);
 	}
